d70fae856625d4c7d17221d1958c1556ea0bef17
Sota <sooota09@gmail.com>
2020-03-27 22:10:20 +0900

    [Update]バリデーション完了

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,9 @@ class Book < ApplicationRecord
 
 belongs_to :user
 
+validates :tile, presence: true
+validates :body, presence: true,
+                 length: {maximum:199}
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true,
+                   length: {minimum:2, maximum:20}
+
   has_many :books, dependent: :destroy
 end
